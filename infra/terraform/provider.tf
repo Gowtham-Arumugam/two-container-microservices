82e1480daf2dbb8117b568b35e36b4ec1c2544ea
gowthamArumugam <gowthamarumugam@outlook.com>
2025-08-16 18:49:26 +0530

    gf

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "=1.10.3"
+  required_version = "=1.12.2"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "=4.40.0"
     }
   }
 }
@@ -15,4 +15,34 @@ provider "azurerm" {
 
   features {}
 }
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.Learn_devops.location
+  resource_group_name = azurerm_resource_group.Learn_devops.name
+  dns_prefix          = "exampleaks1"
 
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "DEV"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
