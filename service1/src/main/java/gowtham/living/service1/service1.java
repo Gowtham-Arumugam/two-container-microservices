a8e7e900652a48e94c3c4f03164dc5fa6f8073c5
Gowtham-Arumugam <gowthamcruise@gmail.com>
2025-08-26 20:23:56 +0530

    ENDPOINTS CHANGED

--- service1/src/main/java/gowtham/living/service1/service1.java
+++ service1/src/main/java/gowtham/living/service1/service1.java
@@ -3,17 +3,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class service1 {
-    @GetMapping("/service1")
+    @GetMapping("/service")
     public String hello() {
         return "Hello from Service 1";
     }
-    @GetMapping("/service1/greet")
+    @GetMapping("/greet")
     public String greet() {
         return "Greetings from Service 1 !!!!";
     }
 
     
-    @GetMapping("/service1/status")
+    @GetMapping("/status")
     public String status() {
         return "Service 1 is running smoothly!";
     } 
--- service2/src/main/java/gowtham/living/service2/service2.java
+++ service2/src/main/java/gowtham/living/service2/service2.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class service2 {
-    @GetMapping("/service2")
+    @GetMapping("/service")
     public String hello() {
         return "Hello from Service 2";
     }
-    @GetMapping("/service2/greet")
+    @GetMapping("/greet")
     public String greet() {
         return "Greetings from Service 2!";
+    }
+    @GetMapping("/status")
+    public String status() {
+        return "Service 2 is up and running!";
     }
 }
